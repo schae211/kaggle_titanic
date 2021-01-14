7177d20fc5a3aeac7ccbf5eff3a414d7f44e789a
schae211 <schae211@msu.edu>
2021-01-14 11:45:53 +0100

    Initial commit

---
+++ titanic/GaussianNB_1.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+from sklearn import svm
+from sklearn.linear_model import Perceptron
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+
+
+test_data = pd.read_csv("train.csv")
+submission_data = pd.read_csv("test.csv")
+
+
+def feature_eng(df):
+    df["Sex"] = np.where(df["Sex"] == "female", 0, 1)
+
+    df = df.fillna(df.median())
+
+    for index in df.index:
+        if df.loc[index, "Embarked"] == "S":
+            df.loc[index, "Embarked"] = 0
+        elif df.loc[index, "Embarked"] == "C":
+            df.loc[index, "Embarked"] = 1
+        else:
+            df.loc[index, "Embarked"] = 2
+
+    return df
+
+features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+test_data = feature_eng(test_data)
+
+evidence = test_data[features].values.tolist()
+labels = test_data["Survived"].values.tolist()
+
+# Split the data randomly into training and test set (test size is 40%)
+X_training, X_testing, y_training, y_testing = train_test_split(
+    evidence, labels, test_size=0.4
+)
+
+# model = Perceptron()
+# model = svm.SVC()
+# model = KNeighborsClassifier(n_neighbors=3)
+model = GaussianNB()
+
+# Fit model
+model.fit(X_training, y_training)
+
+# Make predictions on the testing set
+predictions = model.predict(X_testing)
+
+# Compute how well we performed
+correct = (y_testing == predictions).sum()
+incorrect = (y_testing != predictions).sum()
+total = len(predictions)
+
+# Print results
+print(f"Results for model {type(model).__name__}")
+print(f"Correct: {correct}")
+print(f"Incorrect: {incorrect}")
+print(f"Accuracy: {100 * correct / total:.2f}%")
+
---
+++ titanic/TensorNN_1.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+
+
+test_data = pd.read_csv("train.csv")
+submission_data = pd.read_csv("test.csv")
+
+
+def feature_eng(df):
+    df["Sex"] = np.where(df["Sex"] == "female", 0, 1)
+
+    df = df.fillna(df.median())
+
+    for index in df.index:
+        if df.loc[index, "Embarked"] == "S":
+            df.loc[index, "Embarked"] = 0
+        elif df.loc[index, "Embarked"] == "C":
+            df.loc[index, "Embarked"] = 1
+        else:
+            df.loc[index, "Embarked"] = 2
+
+    return df
+
+features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+test_data = feature_eng(test_data)
+
+evidence = test_data[features].values.tolist()
+labels = test_data["Survived"].values.tolist()
+
+# Split the data randomly into training and test set (test size is 40%)
+X_training, X_testing, y_training, y_testing = train_test_split(
+    evidence, labels, test_size=0.4
+)
+
+# Convert the labels into categorical (what happens here under the hood?)
+y_training = tf.keras.utils.to_categorical(y_training)
+y_testing = tf.keras.utils.to_categorical(y_testing)
+
+# Convert the evidence list into a numpy array (why is that necessary?)
+X_training = np.array(X_training)
+X_testing = np.array(X_testing)
+
+# Create a neural network (sequential neural network in this case)
+model = tf.keras.models.Sequential()
+
+# Add a hidden layer with 24 units, with ReLU activation
+model.add(tf.keras.layers.Dense(14, input_shape=(7,), activation="relu"))
+
+# Add hidden layer with 1 unit, with relu activation
+model.add(tf.keras.layers.Dense(28, activation="relu"))
+
+# Add output layer with 2 units, with sigmoid activation function for the probability
+model.add(tf.keras.layers.Dense(2, activation="sigmoid"))
+
+# Train neural network (how to optimize, which loss function, which metric to evaluate how good the model is)
+model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=["accuracy"]
+)
+# Train the model (epochs=20 means, we will go through each data point 20 times?!)
+model.fit(X_training, y_training, epochs=30)
+
+# Evaluate how well model performs
+model.evaluate(X_testing, y_testing, verbose=2)
