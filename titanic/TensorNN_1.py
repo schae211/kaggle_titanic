69718c14934ab36cbac2b889723e0f8fa05ea243
mx-schafer <max.mf.schaefer@web.de>
2021-01-16 15:47:31 +0100

    renaming test, training, validation, Creation of submission file

--- titanic/TensorNN_1.py
+++ titanic/TensorNN_1.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 import tensorflow.keras.backend as K
 import matplotlib.pyplot as plt
 
-test_data = pd.read_csv("train.csv")
+training_data = pd.read_csv("train.csv")
 submission_data = pd.read_csv("test.csv")
 
 
@@ -64,29 +64,34 @@ def feature_eng(df):
     return df
 
 
-features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+# features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+features = ["Sex", "SibSp", "Parch", "Age"]
 
-test_data = feature_eng(test_data)
+training_data = feature_eng(training_data)
+test_data = feature_eng(submission_data)
 
-evidence = test_data[features].values.tolist()
-labels = test_data["Survived"].values.tolist()
+evidence = training_data[features].values.tolist()
+labels = training_data["Survived"].values.tolist()
+
+test_data = test_data[features].values.tolist()
 
 # Split the data randomly into training and test set (test size is 40%)
-X_training, X_testing, y_training, y_testing = train_test_split(evidence, labels, test_size=0.2)
+X_training, X_validation, y_training, y_validation = train_test_split(evidence, labels, test_size=0.2)
 
 # Convert the labels into categorical (what happens here under the hood?)
 y_training = tf.keras.utils.to_categorical(y_training)
-y_testing = tf.keras.utils.to_categorical(y_testing)
+y_validation = tf.keras.utils.to_categorical(y_validation)
 
 # Convert the evidence list into a numpy array (why is that necessary?)
 X_training = np.array(X_training)
-X_testing = np.array(X_testing)
+X_validation = np.array(X_validation)
+X_test = np.array(test_data)
 
 # Create a neural network (sequential neural network in this case)
 model = tf.keras.models.Sequential()
 
 # Add a hidden layer with 24 units, with ReLU activation
-model.add(tf.keras.layers.Dense(32, input_shape=(7,), activation="relu"))
+model.add(tf.keras.layers.Dense(32, input_shape=(len(features),), activation="relu"))
 
 # Add hidden layer with 1 unit, with relu activation
 model.add(tf.keras.layers.Dense(64, activation="relu"))
@@ -95,18 +100,21 @@ model.add(tf.keras.layers.Dense(64, activation="relu"))
 model.add(tf.keras.layers.Dense(2, activation="sigmoid"))
 
 # Train neural network (how to optimize, which loss function, which metric to evaluate how good the model is)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001, name='Adam')
+
 model.compile(
-    optimizer="adam",
+    optimizer=optimizer,
+    # optimizer="adam",
     # loss="binary_crossentropy",
     loss=binary_focal_loss(),
     metrics=["accuracy"]
 )
 # Train the model (epochs=20 means, we will go through each data point 20 times?!)
 training_history = model.fit(X_training, y_training, epochs=100,
-                             validation_data=(X_testing, y_testing))
+                             validation_data=(X_validation, y_validation), batch_size=16)
 
 # Evaluate how well model performs
-model.evaluate(X_testing, y_testing, verbose=2)
+model.evaluate(X_validation, y_validation, verbose=2)
 plt.plot(training_history.history['accuracy'])
 plt.plot(training_history.history['val_accuracy'])
 plt.title('model accuracy')
@@ -115,3 +123,15 @@ plt.xlabel('epoch')
 plt.legend(['train', 'val'], loc='upper left')
 plt.show()
 
+predictions = model.predict(X_test)
+results = np.zeros(len(predictions), dtype=int)
+
+for i in range(len(predictions)):
+    if predictions[i, 0] > predictions[i, 1]:
+        results[i] = 0
+    else:
+        results[i] = 1
+
+# Safe predictions:
+output = pd.DataFrame({'PassengerId': submission_data.PassengerId, 'Survived': results})
+output.to_csv('submission_tensorNN_2.csv', index=False)
