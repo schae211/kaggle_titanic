5155f5e8a0c3c49634449e59fc77ee33064a9266
mx-schafer <max.mf.schaefer@web.de>
2021-01-17 19:03:14 +0100

    refactoring

--- titanic/TensorNN_1.py
+++ titanic/TensorNN_1.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from loss_function import binary_focal_loss_fixed
 from cfg import cfg
 
@@ -43,54 +43,57 @@ def load_and_create_data_partition(train_data, test_data):
     test_data = test_data[features].values.tolist()
 
     # Split the data randomly into training and test set (test size is 40%)
-    X_training, X_validation, y_training, y_validation = train_test_split(evidence, labels,
-                                                                          test_size=0.2,
-                                                                          random_state=False)
+    X_train, X_val, y_train, y_val = train_test_split(evidence, labels, test_size=0.2, random_state=False)
 
     # Convert the labels into categorical (what happens here under the hood?)
-    y_training = tf.keras.utils.to_categorical(y_training)
-    y_validation = tf.keras.utils.to_categorical(y_validation)
+    y_train = tf.keras.utils.to_categorical(y_train)
+    y_val = tf.keras.utils.to_categorical(y_val)
 
     # Convert the evidence list into a numpy array (why is that necessary?)
-    X_training = np.array(X_training)
-    X_validation = np.array(X_validation)
+    X_train = np.array(X_train)
+    X_val = np.array(X_val)
     X_test = np.array(test_data)
-    return X_training, X_validation, X_test, y_training, y_validation
+    return X_train, X_val, X_test, y_train, y_val
 
 
 def create_model():
     # Create a neural network (sequential neural network in this case)
-    model = tf.keras.models.Sequential()
+    nn_model = tf.keras.models.Sequential()
 
     # Add a hidden layer with 24 units, with ReLU activation
-    model.add(tf.keras.layers.Dense(128, input_shape=(len(cfg['training']['features']),), activation="relu",
-                                    kernel_regularizer=tf.keras.regularizers.l1(0.001),
-                                    activity_regularizer=tf.keras.regularizers.l2(0.001), use_bias=False))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.Dropout(0.4))
+    nn_model.add(tf.keras.layers.Dense(128, input_shape=(len(cfg['training']['features']),), activation="relu",
+                                       kernel_regularizer=tf.keras.regularizers.l1(cfg['network']['l1']),
+                                       activity_regularizer=tf.keras.regularizers.l2(cfg['network']['l2']),
+                                       use_bias=False))
+    nn_model.add(tf.keras.layers.BatchNormalization())
+    nn_model.add(tf.keras.layers.Dropout(0.4))
 
-    # Add hidden layer with 1 unit, with relu activation
-    model.add(tf.keras.layers.Dense(64, activation="relu", kernel_regularizer=tf.keras.regularizers.l1(0.001),
-                                    activity_regularizer=tf.keras.regularizers.l2(0.001), use_bias=False))
-    model.add(tf.keras.layers.BatchNormalization())
-    model.add(tf.keras.layers.Dropout(0.4))
+    for _ in range(cfg['network']['hidden_layer']):
+        # Add hidden layer with 1 unit, with relu activation
+        nn_model.add(tf.keras.layers.Dense(cfg['network']['hidden_neurons'], activation="relu",
+                                           kernel_regularizer=tf.keras.regularizers.l1(cfg['network']['l1']),
+                                           activity_regularizer=tf.keras.regularizers.l2(cfg['network']['l2']),
+                                           use_bias=False))
+        nn_model.add(tf.keras.layers.BatchNormalization())
+        nn_model.add(tf.keras.layers.Dropout(0.4))
 
     # Add output layer with 2 units, with sigmoid activation function for the probability
-    model.add(tf.keras.layers.Dense(2, activation="sigmoid", kernel_regularizer=tf.keras.regularizers.l1(0.001),
-                                    activity_regularizer=tf.keras.regularizers.l2(0.001)))
-    model.add(tf.keras.layers.Softmax())
+    nn_model.add(tf.keras.layers.Dense(2, activation="sigmoid",
+                                       kernel_regularizer=tf.keras.regularizers.l1(cfg['network']['l1']),
+                                       activity_regularizer=tf.keras.regularizers.l2(cfg['network']['l2'])))
+    nn_model.add(tf.keras.layers.Softmax())
 
     # Train neural network (how to optimize, which loss function, which metric to evaluate how good the model is)
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001, name='Adam')
+    optimizer = tf.keras.optimizers.Adam(learning_rate=cfg['training']['learning_rate'], name='Adam')
 
-    model.compile(
+    nn_model.compile(
         optimizer=optimizer,
         # optimizer="adam",
         # loss="binary_crossentropy",
         loss=binary_focal_loss_fixed,
         metrics=["accuracy"])
-    model.summary()
-    return model
+    nn_model.summary()
+    return nn_model
 
 
 def plot_training_history(history):
@@ -105,27 +108,29 @@ def plot_training_history(history):
 
 
 if __name__ == "__main__":
-    training_data = pd.read_csv("train.csv")
-    submission_data = pd.read_csv("test.csv")
+    training_data = pd.read_csv("Data/train.csv")
+    submission_data = pd.read_csv("Data/test.csv")
     X_training, X_validation, X_test, y_training, y_validation = load_and_create_data_partition(training_data,
                                                                                                 submission_data)
 
     # callbacks
-    earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
+    earlyStopping = EarlyStopping(monitor='val_loss', patience=cfg['training']['patience'], verbose=0, mode='min')
     mcp_save = ModelCheckpoint('./Models/best_model.h5', save_best_only=True, monitor='val_loss', mode='min')
-    reduce_lr_loss = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=7, verbose=1, epsilon=1e-4, mode='min')
 
     # Train the model (epochs=20 means, we will go through each data point 20 times?!)
     model = create_model()
-    training_history = model.fit(X_training, y_training, epochs=500,
-                                 validation_data=(X_validation, y_validation), batch_size=16,
-                                 callbacks=[earlyStopping, mcp_save, reduce_lr_loss])
+    training_history = model.fit(X_training, y_training, epochs=cfg['training']['epochs'],
+                                 validation_data=(X_validation, y_validation), batch_size=cfg['training']['batch_size'],
+                                 callbacks=[earlyStopping, mcp_save])
     model.evaluate(X_validation, y_validation, verbose=2)
     plot_training_history(training_history)
 
     # custom objects to load the custom loss function is needed
     model = tf.keras.models.load_model("./Models/best_model.h5",
                                        custom_objects={'binary_focal_loss_fixed': binary_focal_loss_fixed})
+
+    # TODO: Create confususion matrix -> False Positive, True pos, Tre neg, False neg
+    #      - evaluation function using validation
 
     predictions = model.predict(X_test)
     results = np.zeros(len(predictions), dtype=int)
@@ -138,4 +143,4 @@ if __name__ == "__main__":
 
     # Safe predictions:
     output = pd.DataFrame({'PassengerId': submission_data.PassengerId, 'Survived': results})
-    output.to_csv('submission_tensorNN_2.csv', index=False)
+    output.to_csv('./submissions/submission_tensorNN_2.csv', index=False)
--- titanic/cfg.py
+++ titanic/cfg.py
@@ -6,7 +6,14 @@ training_parameter = {"epochs": 200,
                       "learning_rate": 0.0001,
                       "alpha": 0.25,
                       "gamma": 2.0,
-                      "features": ["Sex", "SibSp", "Age", "Pclass", "Parch", "Fare"]}
+                      "features": ["Sex", "SibSp", "Age", "Pclass", "Parch", "Fare"],
+                      "batch_size": 16,
+                      "patience": 10}
+
+network_parameter = {"hidden_layer": 1,
+                     "hidden_neurons": 64,
+                     "l1": 0.001,
+                     "l2": 0.001}
 
 
-cfg = {'training': training_parameter, 'network': dict()}
+cfg = {'training': training_parameter, 'network': network_parameter}
